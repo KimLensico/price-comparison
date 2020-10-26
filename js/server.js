70d3337707bb5715b153f548b6ea6c3fb1753594
Garrett Cintron <grrttcintron@gmail.com>
2020-10-26 14:44:54 -0700

    constructor updated. homepagerender not finished but updated. error route added. error-view(ejs) added 1 server constant added(gamesArray)

--- js/server.js
+++ js/server.js
@@ -1,5 +1,8 @@
 'use strict';
 
+//server constants
+let gamesArray = [];
+
 // require third party dependencies
 require('dotenv').config();
 const express = require('express');
@@ -12,7 +15,7 @@ const PORT = process.env.PORT || 3000;
 // setting up front end viewport & configs
 app.set('view engine', 'ejs');
 app.use(express.static('./public'));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 // connecting database
 const client = new pg.Client(process.env.DATABASE_URL);
@@ -23,7 +26,7 @@ app.listen(PORT, () => {
   console.log(`Server is listening at: ${PORT}`);
 })
 
-// TESTING BEGIN: to make sure we can grab API data 
+// TESTING BEGIN: to make sure we can grab API data
 const testAPI = (req, res) => {
   let url = `https://www.cheapshark.com/api/1.0/deals`;
   superagent.get(url)
@@ -33,4 +36,38 @@ const testAPI = (req, res) => {
 app.get('/test', testAPI);
 // TESTING END
 
+app.get('/', RenderListOfGamesonHomepage);
 
+function renderHomepage(req, res) {
+  res.render('homepage-view');
+}
+
+function RenderListOfGamesonHomepage(req, res) {
+  let url = `https://www.cheapshark.com/api/1.0/deals`;
+  superagent.get(url)
+    .then(data => {
+      console.log(data.body[0].title);
+      let gamesToBeRendered = data.body;
+      console.log(gamesToBeRendered);
+      // console.log(gamesToBeRendered)
+      let makingAList = gamesToBeRendered.map((game) => (new Games(game)));
+      console.log(makingAList)
+      res.render('homepage-view', { gamesArray : makingAList })
+    })
+    .catch(err => {
+      console.error(err);
+      res.render('error-view', { error: err });
+    });
+}
+
+function Games(gamesData) {
+  this.title = gamesData.title;
+  this.thumbnail = gamesData.thumb;
+  this.steamRating = gamesData.steamRatingPercent;
+  this.dealRating = gamesData.dealRating;
+  this.storeID = gamesData.storeID;
+  gamesArray.push(gamesData);
+}
+
+//error message
+app.get('*', (req, res) => res.status(404).send('nobody is home'));
