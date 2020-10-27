69390fcde31af4d8ee9f8e29fb1b66a32ba08c68
Aysia Brown <aysiaebrown@gmail.com>
2020-10-26 18:01:29 -0700

    updated sql

--- data/schema.sql
+++ data/schema.sql
@@ -1,6 +1,18 @@
-DROP TABLE IF EXISTS books;
+DROP TABLE IF EXISTS games;
 
-CREATE TABLE books (
+CREATE TABLE games (
   id SERIAL PRIMARY KEY,
-  
+  title VARCHAR(255),
+  thumbnail VARCHAR(255),
+  game_rating INTEGER,
+  deal_rating NUMERIC(2,3),
+  store_id INTEGER,
+  normal_price VARCHAR(255),
+  sale_price VARCHAR(255),
 );
+
+CREATE TABLE popular_titles (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(255),
+  thumbnail VARCHAR(255),
+)
